80c8571f69b52047ccd931bc55e6c80cd229793e
Adam Czajkowski <prawilny@unoperate.com>
2021-09-21 11:38:50 +0200

    MirroringAsyncConfiguration: comment + formatter

--- bigtable-hbase-mirroring-client-2.x-parent/bigtable-hbase-mirroring-client-2.x/src/test/java/com/google/cloud/bigtable/mirroring/hbase2_x/TestMirroringAsyncConfiguration.java
+++ bigtable-hbase-mirroring-client-2.x-parent/bigtable-hbase-mirroring-client-2.x/src/test/java/com/google/cloud/bigtable/mirroring/hbase2_x/TestMirroringAsyncConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.bigtable.mirroring.hbase2_x;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -8,17 +23,13 @@ import com.google.cloud.bigtable.mirroring.hbase1_x.MirroringConnection;
 import com.google.cloud.bigtable.mirroring.hbase1_x.utils.MirroringConfigurationHelper;
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
-import org.junit.function.ThrowingRunnable;
 
 public class TestMirroringAsyncConfiguration {
   private Exception assertInvalidConfiguration(final Configuration test) {
     return assertThrows(
         IllegalArgumentException.class,
-        new ThrowingRunnable() {
-          @Override
-          public void run() throws Throwable {
-            new MirroringAsyncConfiguration(test);
-          }
+        () -> {
+          new MirroringAsyncConfiguration(test);
         });
   }
 
@@ -112,11 +123,8 @@ public class TestMirroringAsyncConfiguration {
         new MirroringAsyncConfiguration(emptyMirroringConfiguration);
     assertThrows(
         IllegalArgumentException.class,
-        new ThrowingRunnable() {
-          @Override
-          public void run() throws Throwable {
-            new MirroringConfiguration(configuration);
-          }
+        () -> {
+          new MirroringConfiguration(configuration);
         });
   }
 
