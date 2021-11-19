21e3f8eceb157eb199d165c78fc8d1e756c950cd
Adam Czajkowski <prawilny@unoperate.com>
2021-11-19 17:11:50 +0100

    Revert "TestMirroringBufferedMutator: check that mutates on primary and secondary perform equal number of operations" The next comment requires different approach.
    This reverts commit 9a200cfa250e1af9a5efc6cd622b55cc3dc3dfc5.

--- bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/test/java/com/google/cloud/bigtable/mirroring/hbase1_x/TestHelpers.java
+++ bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/test/java/com/google/cloud/bigtable/mirroring/hbase1_x/TestHelpers.java
@@ -46,7 +46,6 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.RetriesExhaustedWithDetailsException;
 import org.apache.hadoop.hbase.client.Row;
 import org.apache.hadoop.hbase.client.Table;
-import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.exceptions.base.MockitoException;
 import org.mockito.invocation.InvocationOnMock;
@@ -327,18 +326,5 @@ public class TestHelpers {
         return null;
       }
     };
-  }
-
-  public static <T> void assertListCaptorsHaveEqualFlattenedLength(
-      ArgumentCaptor<List<T>> captor1, ArgumentCaptor<List<T>> captor2) {
-    assertThat(sumLengths(captor1.getAllValues())).isEqualTo(sumLengths(captor2.getAllValues()));
-  }
-
-  private static <T> int sumLengths(List<List<T>> lists) {
-    int sum = 0;
-    for (List list : lists) {
-      sum += list.size();
-    }
-    return sum;
   }
 }
