dc0dc9c7d73d9dc37fe8f072e48089ceb76f7fb0
Adam Czajkowski <prawilny@unoperate.com>
2021-11-19 12:16:42 +0100

    TestVerificationSampling: clear up confusing part

--- bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/test/java/com/google/cloud/bigtable/mirroring/hbase1_x/TestVerificationSampling.java
+++ bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/test/java/com/google/cloud/bigtable/mirroring/hbase1_x/TestVerificationSampling.java
@@ -190,6 +190,7 @@ public class TestVerificationSampling {
     mirroringTable.batch(ops);
     verify(readSampler, times(1)).shouldNextReadOperationBeSampled();
     verify(primaryTable, times(1)).batch(eq(ops), any(Object[].class));
+    verify(secondaryTable, times(1)).batch(eq(ImmutableList.of(put)), any(Object[].class));
 
     withSamplingEnabled(true);
     mirroringTable.batch(ops);
@@ -197,7 +198,6 @@ public class TestVerificationSampling {
     verify(readSampler, times(2)).shouldNextReadOperationBeSampled();
     verify(primaryTable, times(2)).batch(eq(ops), any(Object[].class));
     verify(secondaryTable, times(1)).batch(eq(ops), any(Object[].class));
-    verify(secondaryTable, times(1)).batch(eq(ImmutableList.of(put)), any(Object[].class));
   }
 
   @Test
